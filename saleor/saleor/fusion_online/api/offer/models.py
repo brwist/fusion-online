25d65ed9b35c525f72725a50f4d3907c14a23988
Maeghan Provencher <maeghan@bowst.com>
2021-08-03 15:41:06 -0400

    fixes datetimefield issue

--- saleor/saleor/fusion_online/api/offer/models.py
+++ saleor/saleor/fusion_online/api/offer/models.py
@@ -8,7 +8,7 @@ class Offer(models.Model):
 		choices=[("excess_list", "Excess List"), ("stock_list", "Stock List"), ("vendor_offer", "Vendor Offer"), ("rms_offfer", "RMS Offer"), ("po", "PO")]
 	)
 	lead_time_days = models.IntegerField(default=-1)
-	date_added = models.DateTimeField(blank=True, default="")
+	date_added = models.DateTimeField(blank=True, null=True)
 	date_code = models.CharField(max_length=50, blank=True, default="")
 	comment = models.CharField(max_length=300, blank=True, default="")
 	vendor_type = models.CharField(max_length=50, blank=True, default="")
