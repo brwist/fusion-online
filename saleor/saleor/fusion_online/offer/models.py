db0142f29697571dea2be9347426f45d4cfdafc0
Maeghan Provencher <maeghan@bowst.com>
2021-08-06 13:35:48 -0400

    adds tariff field to offer model

--- saleor/saleor/fusion_online/offer/models.py
+++ saleor/saleor/fusion_online/offer/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from ...product.models import ProductVariant
 
+TYPE_CHOICES = [
+	("excess_list", "Excess List"),
+	("stock_list", "Stock List"),
+	("vendor_offer", "Vendor Offer"),
+	("rms_offfer", "RMS Offer"),
+	("po", "PO")]
 class Offer(models.Model):
 	type = models.CharField(
 		max_length=50,
 		blank=True, 
 		default="",
-		choices=[("excess_list", "Excess List"), ("stock_list", "Stock List"), ("vendor_offer", "Vendor Offer"), ("rms_offfer", "RMS Offer"), ("po", "PO")]
+		choices=TYPE_CHOICES
 	)
 	lead_time_days = models.IntegerField(default=-1)
 	date_added = models.IntegerField(null=True)
@@ -14,6 +20,7 @@ class Offer(models.Model):
 	comment = models.CharField(max_length=300, blank=True, default="")
 	vendor_type = models.CharField(max_length=50, blank=True, default="")
 	vendor_region = models.CharField(max_length=50, blank=True, default="")
+	tariff = models.FloatField(null=True, blank=True)
 	product_variant = models.OneToOneField(
 		ProductVariant,
 		on_delete=models.CASCADE
