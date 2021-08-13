6e41abb0294b69bc035768a3fd0ba1358f6eab71
Maeghan Provencher <maeghan@bowst.com>
2021-08-13 12:07:32 -0400

    fixes offers endpoint bug by retrieving product using metadata

--- saleor/saleor/fusion_online/offer/serializers.py
+++ saleor/saleor/fusion_online/offer/serializers.py
@@ -33,7 +33,8 @@ class OfferSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         # Get product
-        product = Product.objects.get(mpn=validated_data["mpn"],item_num_id=validated_data["item_num_id"])
+        product = Product.objects.get(metadata__item_num_id=str(validated_data['item_num_id']))
+        print('product', product)
         print("--RETRIEVED PRODUCT--")
 
         # Get or create variant
