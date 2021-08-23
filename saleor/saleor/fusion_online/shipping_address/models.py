72a43ec3e893b8fe939ff6599400b85ca0120613
Kynan Delorey <kynand@gmail.com>
2021-08-23 12:29:32 -0400

    Update fusion_online shipping address model to make most things nullable. Update some data in the shipping address REST client request file.

--- saleor/saleor/fusion_online/shipping_address/models.py
+++ saleor/saleor/fusion_online/shipping_address/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, CharField, ForeignKey, CASCADE, DO_NOTHING, DateTimeField, BooleanField, Index, IntegerField, TextField, SET_NULL, ManyToManyField
+from django.db.models import Model, CharField, ForeignKey, CASCADE, DateTimeField, BigIntegerField
 
 from ...account.models import Address
 
@@ -6,12 +6,12 @@ from ...account.models import Address
 class ShippingAddress(Model):
     address = ForeignKey(Address, on_delete=CASCADE,
                          max_length=256, blank=True, null=True, related_name="ship_to_address")
-    customer_id = IntegerField(blank=True)
+    customer_id = BigIntegerField(blank=True, null=True)
     ship_to_name = CharField(max_length=256, blank=True)
-    ship_via = CharField(max_length=256, blank=True)
-    vat_id = CharField(max_length=256, blank=True)
-    ship_to_num = IntegerField(blank=True, null=True)
-    validation_message = CharField(max_length=256, blank=True)
+    ship_via = CharField(max_length=256, blank=True, null=True)
+    vat_id = CharField(max_length=256, blank=True, null=True)
+    ship_to_num = BigIntegerField(blank=True, null=True)
+    validation_message = CharField(max_length=256, blank=True, null=True)
 
     created = DateTimeField(auto_now_add=True, null=True)
     updated = DateTimeField(auto_now=True, null=True)
