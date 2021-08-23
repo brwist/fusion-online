a4c6ab3dd72166b5797daa00e464f5e72909217a
Alex Vallejo <alxvallejo@gmail.com>
2021-08-23 16:47:43 -0400

    Load saleor useAuth hook

--- storefront/src/components/MyAccount/Shipping.tsx
+++ storefront/src/components/MyAccount/Shipping.tsx
@@ -2,13 +2,50 @@ import React from 'react';
 import { Row, Col, Card, Button } from 'react-bootstrap';
 import { Tag } from '../Tag/Tag';
 
+import { useAuth, useCreateUserAddress } from '@saleor/sdk';
+
 import './myaccount.scss';
 
 export interface ShippingProps {}
 
-export const Shipping: React.FC<ShippingProps> = ({
-  ...props
-}) => {
+export const Shipping: React.FC<ShippingProps> = ({ ...props }) => {
+  const { user, loaded } = useAuth();
+  console.log('user: ', user);
+
+  const renderAddress = (address: any) => {
+    return (
+      <Card>
+        <Card.Body>
+          <Row>
+            <Col>
+              <div className="mb-2">
+                Full Name
+                <br />
+                123 Main St.
+                <br />
+                City, State 01234, US
+                <br />
+                (000) 000-0000
+              </div>
+              <div>
+                <Button variant="link" className="small px-0">
+                  EDIT ADDRESS
+                </Button>{' '}
+                |{' '}
+                <Button variant="link" className="small px-0">
+                  REMOVE ADDRESS
+                </Button>
+              </div>
+            </Col>
+            <Col className="text-right">
+              <Tag size="sm" label="Default" />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
     <div className="shipping">
       <header className="my-3 d-flex justify-content-between align-items-center">
@@ -20,13 +57,22 @@ export const Shipping: React.FC<ShippingProps> = ({
           <Row>
             <Col>
               <div className="mb-2">
-                Full Name<br />
-                123 Main St.<br />
-                City, State 01234, US<br />
+                Full Name
+                <br />
+                123 Main St.
+                <br />
+                City, State 01234, US
+                <br />
                 (000) 000-0000
               </div>
               <div>
-                <Button variant="link" className="small px-0">EDIT ADDRESS</Button> | <Button variant="link" className="small px-0">REMOVE ADDRESS</Button>
+                <Button variant="link" className="small px-0">
+                  EDIT ADDRESS
+                </Button>{' '}
+                |{' '}
+                <Button variant="link" className="small px-0">
+                  REMOVE ADDRESS
+                </Button>
               </div>
             </Col>
             <Col className="text-right">
@@ -41,13 +87,22 @@ export const Shipping: React.FC<ShippingProps> = ({
           <Row>
             <Col>
               <div className="mb-2">
-                Full Name<br />
-                123 Main St.<br />
-                City, State 01234, US<br />
+                Full Name
+                <br />
+                123 Main St.
+                <br />
+                City, State 01234, US
+                <br />
                 (000) 000-0000
               </div>
               <div>
-                <Button variant="link" className="small px-0">EDIT ADDRESS</Button> | <Button variant="link" className="small px-0">REMOVE ADDRESS</Button>
+                <Button variant="link" className="small px-0">
+                  EDIT ADDRESS
+                </Button>{' '}
+                |{' '}
+                <Button variant="link" className="small px-0">
+                  REMOVE ADDRESS
+                </Button>
               </div>
             </Col>
           </Row>
