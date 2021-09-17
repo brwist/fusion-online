b23503c95a9b0f9b7c8dd3bdec7978bbdc8b0ca6
Maeghan Provencher <maeghan@bowst.com>
2021-09-17 14:30:19 -0400

    updates offers post endpoint to use new vendor model

--- saleor/saleor/fusion_online/offer/serializers.py
+++ saleor/saleor/fusion_online/offer/serializers.py
@@ -1,90 +1,22 @@
+from enum import unique
+from django.db.models import fields
 from rest_framework import serializers
-from ..offer.models import Offer, VENDOR_REGION_CHOICES, VENDOR_TYPE_CHOICES
-from ...product.models import Product, ProductVariant, Attribute, AttributeValue
-from ...product.utils.attributes import associate_attribute_values_to_instance
-from ...warehouse.models import Warehouse, Stock
+from ..offer.models import Offer, Vendor, VENDOR_REGION_CHOICES, VENDOR_TYPE_CHOICES
 
-TYPE_CHOICES = [
-    ("EXCESS_LIST", "Excess List"),
-    ("STOCK_LIST", "Stock List"),
-    ("VENDOR_OFFER", "Vendor Offer"),
-    ("RMS_OFFER", "RMS Offer"),
-    ("PO", "PO")]
+class VendorSerializer(serializers.ModelSerializer):
+    vendor_number = serializers.IntegerField(error_messages = {
+        "null": "'source' field cannot be null",
+        "invalid": "'source' field must be a valid integer"
+    })
+    company = serializers.CharField(source="vendor_name")
+    class Meta:
+        model = Vendor
+        fields = ['company', 'vendor_number', 'vendor_type', 'vendor_region']
     
-class OfferSerializer(serializers.Serializer):
-    type = serializers.ChoiceField(choices=TYPE_CHOICES)
-    date_added = serializers.IntegerField(max_value=None, min_value=None)
-    source = serializers.IntegerField(max_value=None, min_value=None)
-    company = serializers.CharField(max_length=50)
-    item_num_id = serializers.IntegerField(max_value=None, min_value=None)
-    mpn = serializers.CharField(max_length=50)
-    mcode = serializers.CharField(max_length=10)
-    quantity = serializers.IntegerField(max_value=None, min_value=0)
-    offer_price = serializers.CharField(max_length=20)
-    date_code = serializers.CharField(max_length=50, required=False, allow_blank=True, default="")
-    comment = serializers.CharField(max_length=500, required=False, allow_blank=True, default="")
-    coo = serializers.CharField(max_length=60, required=False, allow_blank=True, default="")
-    lead_time_days = serializers.IntegerField(max_value=None, min_value=-1)
-    offer_id = serializers.IntegerField(max_value=None, min_value=None)
-    vendor_type = serializers.ChoiceField(VENDOR_TYPE_CHOICES)
-    vendor_region = serializers.ChoiceField(VENDOR_REGION_CHOICES)
-    tariff_rate = serializers.FloatField(required=False, allow_null=True, default=0) 
-    coo = serializers.CharField(max_length=60, required=False, allow_blank=True, default="") 
 
-    def create(self, validated_data):
-        # Get product
-        product = Product.objects.get(metadata__item_num_id=validated_data['item_num_id'])
+class OfferSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Offer
+        exclude = ['product_variant']
 
-        print("--RETRIEVED PRODUCT--")
 
-        # Get or create variant
-        product_variant = ProductVariant.objects.get_or_create(
-            sku=validated_data["offer_id"],
-            product=product,
-            price_amount=validated_data["offer_price"]
-        )
-        print("--RETRIEVED OR CREATED VARIANT--")
-
-        # get or create vendor attribute value
-        attribute_vendor = Attribute.objects.get(slug="vendor")
-        try: 
-            attribute_vendor_value = AttributeValue.objects.get(
-                slug=validated_data["source"],
-                attribute=attribute_vendor
-            )
-            print("--RETRIEVED VENDOR--")
-        except AttributeValue.DoesNotExist: 
-            attribute_vendor_value = AttributeValue.objects.create(
-                name=validated_data["company"],
-                slug=validated_data["source"],
-                attribute=attribute_vendor
-            )
-            print("--CREATED NEW VENDOR--")
-
-        # associate vendor attribute value to product variant
-        associate_attribute_values_to_instance(product_variant[0], attribute_vendor, attribute_vendor_value)
-        print("--ASSOCIATED VENDOR TO VARIANT--")
-
-        # get warehouse and store quantity
-        warehouse = Warehouse.objects.get(name="Test Warehouse")
-        Stock.objects.create(
-            warehouse=warehouse,
-            product_variant=product_variant[0],
-            quantity=validated_data['quantity'])
-        print("--QUANTITY ADDED TO VARIANT--")
-
-        # create offer
-        offer_data = {
-            "type": validated_data["type"],
-            "date_added": validated_data["date_added"],
-            "date_code": validated_data["date_code"],
-            "comment": validated_data["comment"],
-            "vendor_type": validated_data["vendor_type"],
-            "vendor_region": validated_data["vendor_region"],
-            "lead_time_days": validated_data["lead_time_days"],
-            "product_variant": product_variant[0],
-            "tariff_rate": validated_data["tariff_rate"],
-            "coo": validated_data["coo"]
-        }
-        print("SAVING OFFER...")
-        return Offer.objects.create(**offer_data)
--- saleor/saleor/fusion_online/offer/views.py
+++ saleor/saleor/fusion_online/offer/views.py
@@ -1,25 +1,40 @@
 from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.decorators import api_view, throttle_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.throttling import UserRateThrottle
-from .serializers import OfferSerializer
+from .serializers import OfferSerializer, VendorSerializer
+from .models import Vendor
 from django.db import transaction
 
 @api_view(['POST'])
 @throttle_classes([UserRateThrottle])
 @transaction.atomic
 def handler(request):
-	data = JSONParser().parse(request)
+    try: 
+        data = JSONParser().parse(request)
+        vendor_data = {
+            'company': data.pop('company', None),
+            'vendor_number': data.pop('source', None),
+            'vendor_type': data.pop('vendor_type', None),
+            'vendor_region': data.pop('vendor_region', None)
+        }
+        
+        vendor_serializer = VendorSerializer(data=vendor_data)
+        
+        if vendor_serializer.is_valid():
+            vendor = Vendor.objects.get_or_create(vendor_serializer.data)
+        else:
+            return JsonResponse(vendor_serializer.errors, status=400)
 
-	serializer = OfferSerializer(data=data)
+        data['vendor'] = vendor[0].pk
+        offer_serializer = OfferSerializer(data=data)
+        if offer_serializer.is_valid():
+            result = offer_serializer.save()
+            return Response({'fo_ref_id': result.pk}, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(offer_serializer.errors, status=400)
 
-	try:
-		with transaction.atomic():
-			if not serializer.is_valid():
-				return JsonResponse(serializer.errors, status=400)
-			else:
-				result = serializer.save()
-				return Response({"fo_ref_id": result.pk})
-	except Exception as e:
-		return Response({"error": True, "message": str(e)}, status=500)
+    except Exception as e:
+        return Response({"error": True, "message": str(e)}, status=500)
