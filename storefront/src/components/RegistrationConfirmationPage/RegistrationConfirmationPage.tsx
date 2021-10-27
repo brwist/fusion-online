fe840d8905ab28c5af4410ab80050819c2402143
Maeghan Provencher <maeghan@bowst.com>
2021-10-27 14:40:34 -0400

    updates registration confimation page styles to include landing page banner and footer

--- storefront/src/components/RegistrationConfirmationPage/RegistrationConfirmationPage.tsx
+++ storefront/src/components/RegistrationConfirmationPage/RegistrationConfirmationPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom'
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Row } from 'react-bootstrap';
 
 import { SectionHeader } from '../SectionHeader/SectionHeader';
+import { Footer } from '../Footer/Footer';
 
 import './registrationconfirmationpage.scss'
 
@@ -11,10 +12,24 @@ export interface RegistrationConfirmationPageProps {}
 export const RegistrationConfirmationPage: React.FC<RegistrationConfirmationPageProps> = ({}) => {
   const history = useHistory()
   return (
-    <Container>
-      <SectionHeader subheading="Confirmation" heading="Thank you for registering"/>
-      <h6>Please check your email to verify your account</h6>
-      <Button onClick={() => history.push('/')}>Return to Home</Button>
-    </Container>
+    <div className="home">
+      <div className="hero">
+        <Container>
+          <Row>
+            <SectionHeader subheading="Confirmation" heading="Thank you for registering" borderClass="" />
+          </Row>
+        </Container>
+      </div>
+
+      <Container className="content">
+          <Row>
+          <h4>Please check your email to verify your account</h4>
+          </Row>
+          <Row>
+          <Button onClick={() => history.push('/')}>Return to Home</Button>
+          </Row>
+          </Container>
+      <Footer />
+    </div>
   )
 }
