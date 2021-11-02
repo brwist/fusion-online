728ec244e6c39f1b57fbeeb989d2195d856b92e7
Alex Vallejo <alxvallejo@gmail.com>
2021-11-02 14:33:55 -0400

    Send sns notification with order details

--- saleor/saleor/plugins/fusion_online/plugin.py
+++ saleor/saleor/plugins/fusion_online/plugin.py
@@ -3,6 +3,10 @@ from saleor.plugins.base_plugin import BasePlugin
 from ...fusion_online.hubspot.serializers import HubspotContactSerializer
 
 from saleor.fusion_online.hubspot.email import HubspotEmails
+from saleor.fusion_online.notifications.utils import send_sales_order_notification
+from saleor.fusion_online.orders.serializers import SalesOrderSerializer
+
+import json
 
 
 class FusionOnlinePlugin(BasePlugin):
@@ -16,6 +20,11 @@ class FusionOnlinePlugin(BasePlugin):
 
     def order_fully_paid(self, order, previous_value):
 
+        # SNS notification
+        order_serialized = SalesOrderSerializer(order)
+        send_sales_order_notification(order_serialized.data)
+
+        # Confirmation Email
         hubspot_email = HubspotEmails()
         hubspot_email.send_order_confirmation(
             order)
