df3c5da625068909b3411d23f34feb6e15f9e6b4
Maeghan Provencher <maeghan@bowst.com>
2021-11-05 12:33:41 -0400

    retrieve product by item_master_id in csv

--- saleor/saleor/fusion_online/admin/views.py
+++ saleor/saleor/fusion_online/admin/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .forms import UploadFileForm
+from ...product.models import Product
 import csv
 
 def decode_utf8(input_iterator):
@@ -8,12 +8,19 @@ def decode_utf8(input_iterator):
         yield l.decode('utf-8')
 
 def upload_file(request):
-    print("request files", request.FILES)
-    if request.method == 'POST':
-        product_data = csv.DictReader(decode_utf8(request.FILES['product_list_csv']))
-        for row in product_data:
-            print(row)
-
-        return HttpResponse("success")
-    else:
-        return HttpResponseRedirect('/')
+    try:
+        if request.method == 'POST':
+            product_data = csv.DictReader(decode_utf8(request.FILES['product_list_csv']))
+            for row in product_data:
+                item_master_id = int(row['item_master_id'])
+                print("--row data--", row)
+                try:
+                    product = Product.objects.get(metadata__contains={'item_master_id': item_master_id})
+                    print("--product--", product)
+                except Product.DoesNotExist:
+                    return HttpResponse(f'error: No product with item_master_id {item_master_id}', status=400)
+            return HttpResponse("success")
+        else:
+            return HttpResponseRedirect('/')
+    except Exception as e:
+        return HttpResponse(f'error: {str(e)}', status=500)    
