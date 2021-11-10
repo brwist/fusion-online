37b04f1da666526a4409f05921a225e23d0e6c2b
Maeghan Provencher <maeghan@bowst.com>
2021-11-10 13:26:49 -0500

    adds list of metadata and attribute global and cpu

---
+++ saleor/saleor/fusion_online/admin/data/attributes.py
@@ -0,0 +1,19 @@
+attributes = {
+  "global": [
+    "brand",
+    "commodity",
+    "manufacturer",
+    "segment"
+  ],
+  "cpu": [
+    "cpu_core_family",
+    "cpu_lithography",
+    "cpu_max_number_of_mem_channels",
+    "cpu_max_turbo_frequency",
+    "cpu_memory_speed",
+    "cpu_memory_types",
+    "cpu_number_of_cores",
+    "cpu_series",
+    "cpu_variant_name"
+  ]
+}
---
+++ saleor/saleor/fusion_online/admin/data/metadata.py
@@ -0,0 +1,25 @@
+metadata = {
+  "global": [
+    "sku"
+    "mcode",
+    "mpn",
+    "model",
+    "msrp",
+    "coo"
+  ],
+  "cpu": [
+    "cpu_ordering_code_tray",
+    "cpu_ordering_code_retail",
+    "cpu_number_of_threads",
+    "cpu_base_frequency",
+    "cpu_cache",
+    "cpu_max_memory_size",
+    "cpu_memory_bandwith",
+    "cpu_socket",
+    "cpu_socket_count",
+    "cpu_pci_express_version",
+    "cpu_express_lanes",
+    "cpu_tdp",
+    "cpu_workload_affinity",
+  ]
+}
